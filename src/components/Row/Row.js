124fb5b9cecf71fd7dfb7142554028c77356271e
Raja1919 <rajaaart51@gmail.com>
2023-08-04 12:34:38 +0530

    first commit

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./Row.css";
 import axios from "../../Helpers/axios";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const Row = ({ title, fetchUrl, isLarge = false }) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -11,12 +14,37 @@ const Row = ({ title, fetchUrl, isLarge = false }) => {
     const fetchData = async () => {
       const data = await axios.get(fetchUrl);
       setMovies(data.data.results);
-
-      return data;
     };
     fetchData();
   }, [fetchUrl]);
-  console.log(movies);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(
+        movie?.name ||
+          movie?.title ||
+          movie?.id ||
+          movie?.original_name ||
+          movie?.original_title
+      )
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -33,10 +61,12 @@ const Row = ({ title, fetchUrl, isLarge = false }) => {
                   isLarge ? movie.poster_path : movie.backdrop_path
                 }`}
                 alt=""
+                onClick={() => handleClick(movie)}
               />
             )
         )}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
